0ab3e5400d30c645da66b3d701930e88d4e681ae
Lasana Murray <lmurray@quenk.com>
2017-08-02 03:13:46 -0400

    chore: add Source to type

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,17 @@
+export declare interface Source<A> {
+
+    [key: string]: A
+
+}
+
 export declare interface get {
 
-    <A>(path: string, o: object): A;
+    <A>(path: string, o: Source<A>): A;
 
 }
 
 export declare interface set {
 
-    <A>(path: string, value: A, o: object): object;
+    <A>(path: string, value: A, o: Source<A>): object;
 
 }
